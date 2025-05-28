94af6747abe6e72f2fa76d46e9bce0fb7b8619a5
nimuvik <nika.murash@gmail.com>
2025-05-28 12:25:41 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import mvexam
 
-# Register your models here.
+@admin.register(mvexam)
+class ExamAdmin(admin.ModelAdmin):
+    list_displads = ('name', 'users__email')
+    list_filter = ('is_public', 'created_at')
+    date_hiery = ('name', 'created_at', 'exam_date', 'is_public')
+    search_fielarchy = 'exam_date'
+    filter_horizontal = ('users',)
