00e0c8d2bb442c3ad286876cb81118284de8f4a7
nimuvik <nika.murash@gmail.com>
2025-05-28 12:25:29 +0300

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class mvexam(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Название экзамена")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    exam_date = models.DateField(verbose_name="Дата проведения экзамена")
+    image = models.ImageField(upload_to='exam_images/', verbose_name="Изображение задания")
+    users = models.ManyToManyField(User, verbose_name="Пользователи")
+    is_public = models.BooleanField(default=False, verbose_name="Опубликовано")
+
+    def __str__(self):
+        return self.title
