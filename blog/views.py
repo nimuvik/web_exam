9945a03d147dd5b37fc21b38d1845fe9513a297f
nimuvik <nika.murash@gmail.com>
2025-05-28 12:26:37 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import vmexam
 
-# Create your views here.
+def exam_list(request):
+    exams = vmexam.objects.filter(is_public=True)  # Только опубликованные
+    return render(request, 'exams/exam_list.html', {
+        'exams': exams,
+        'fio': 'Вероника Мурашкина',
+        'group': '241-672',
+    })
